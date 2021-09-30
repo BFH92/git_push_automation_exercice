9d2fb92ad9b6883037d9df3e0cf7be7261db16c1
Benoit Figea <60694673+BFH92@users.noreply.github.com>
2021-09-30 14:19:38 +0200

    push readme.md - 2021-09-30 14:19:38 +0200

--- index.rb
+++ index.rb
@@ -1,11 +1,8 @@
 require_relative 'add_text_to_readme'
 require_relative 'push_on_github'
 
-random = [2,4,6,8,10,12]
 date = Time.now
 file_name = "readme.md"
 
-random.sample.times do
-  add_text_to_readme(date, file_name)
-  push_on_github(date, file_name)
-end
+add_text_to_readme(date, file_name)
+push_on_github(date, file_name)
