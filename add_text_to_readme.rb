bede27ff06c4a8af0f2d579a7ea95b210fcc9c60
BFH93 <60694673+BFH92@users.noreply.github.com>
2021-10-04 12:53:01 +0200

    push readme.md - 2021-10-04 12:53:01 +0200

--- add_text_to_readme.rb
+++ add_text_to_readme.rb
@@ -2,9 +2,11 @@ def add_text_to_readme(date, file_name)
 
   file = File.open("#{file_name}", "w")
   file.write("### Les contributions ne se valent pas toutes !\n")
-  file.write("Les développeurs (même juniors) peuvent travailler tous les jours sans push sur GitHub. L'habit ne fait pas le moine...\n")
+  file.write("Les développeurs (même juniors) peuvent travailler tous les jours sans push sur GitHub.#LhabitNeFaitPasLeMoine\n")
   file.write("#### Voici une automatisation pour que votre fil de contributions soit vert tous les jours \n")
-  
+  file.write("#####-------INSTALL------")
+  file.write("Installation d'un cron \n")
+  file.write("$crontab -e \n ")
   file.write("--------------------")
   file.write("\ndernière contribution automatique - #{date}")
   file.close
--- readme.md
+++ readme.md
@@ -1,5 +1,7 @@
 ### Les contributions ne se valent pas toutes !
-Les développeurs (même juniors) peuvent travailler tous les jours sans push sur GitHub. L'habit ne fait pas le moine...
+Les développeurs (même juniors) peuvent travailler tous les jours sans push sur GitHub.#LhabitNeFaitPasLeMoine
 #### Voici une automatisation pour que votre fil de contributions soit vert tous les jours 
---------------------
-dernière contribution automatique - 2021-10-04 12:43:00 +0200
+#####-------INSTALL------Installation d'un cron 
+$crontab -e 
+ --------------------
+dernière contribution automatique - 2021-10-04 12:53:01 +0200
