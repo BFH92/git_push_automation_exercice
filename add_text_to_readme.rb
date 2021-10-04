7014ef825ccde7d730f55c70574de1f5313f8dda
BFH93 <60694673+BFH92@users.noreply.github.com>
2021-10-04 13:03:16 +0200

    push readme.md - 2021-10-04 13:03:16 +0200

--- add_text_to_readme.rb
+++ add_text_to_readme.rb
@@ -4,10 +4,24 @@ def add_text_to_readme(date, file_name)
   file.write("### Les contributions ne se valent pas toutes !\n")
   file.write("Les développeurs (même juniors) peuvent travailler tous les jours sans push sur GitHub.#LhabitNeFaitPasLeMoine\n")
   file.write("#### Voici une automatisation pour que votre fil de contributions soit vert tous les jours \n")
-  file.write("#####-------INSTALL------")
-  file.write("Installation d'un cron \n")
+  file.write("#####Installation")
+  
+  file.write("1/ Installation d'un cron \n")
   file.write("$crontab -e \n ")
-  file.write("--------------------")
+  file.write("insérer ensuite la ligne suivante en spécifiant le chemin où se trouve le repo que vous avez cloné")
+  file.write("* * * * * /bin/bash -l -c 'cd /Users/Path/to/your/directory && ruby ~/your_directory/index.rb' &> /Users/Path/to/your/directory/cron.txt\n ")
+  file.write("remplacer les '* * * * *' pour ajuster la fréquence de push. Vous pouvez tester le format ici => https://crontab.tech/\n")
+  
+  file.write("2/ création de la variable d'environnement Token, pour permettre le push via un gitHub token\n")
+  file.write("création d'un fichier .env\n ")
+  file.write("ajouter la variable TOKEN='Votre tokenGitHub'\n ")
+  file.write("informations sur le gitHub Token ici => https://techglimpse.com/git-push-github-token-based-passwordless/'\n ")
+  file.write("$installation de la gem dotenv\n ")
+  file.write("$ gem install dotenv\n ")
+  
+  file.write("Et vous êtes bons ! \n ")
+  
+  file.write("-----Metrics-----")
   file.write("\ndernière contribution automatique - #{date}")
   file.close
 end
--- readme.md
+++ readme.md
@@ -1,7 +1,16 @@
 ### Les contributions ne se valent pas toutes !
 Les développeurs (même juniors) peuvent travailler tous les jours sans push sur GitHub.#LhabitNeFaitPasLeMoine
 #### Voici une automatisation pour que votre fil de contributions soit vert tous les jours 
-#####-------INSTALL------Installation d'un cron 
+#####Installation1/ Installation d'un cron 
 $crontab -e 
- --------------------
-dernière contribution automatique - 2021-10-04 12:53:01 +0200
+ insérer ensuite la ligne suivante en spécifiant le chemin où se trouve le repo que vous avez cloné* * * * * /bin/bash -l -c 'cd /Users/Path/to/your/directory && ruby ~/your_directory/index.rb' &> /Users/Path/to/your/directory/cron.txt
+ remplacer les '* * * * *' pour ajuster la fréquence de push. Vous pouvez tester le format ici => https://crontab.tech/
+2/ création de la variable d'environnement Token, pour permettre le push via un gitHub token
+création d'un fichier .env
+ ajouter la variable TOKEN='Votre tokenGitHub'
+ informations sur le gitHub Token ici => https://techglimpse.com/git-push-github-token-based-passwordless/'
+ $installation de la gem dotenv
+ $ gem install dotenv
+ Et vous êtes bons ! 
+ -----Metrics-----
+dernière contribution automatique - 2021-10-04 13:03:16 +0200
