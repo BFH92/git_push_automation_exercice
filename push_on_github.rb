f52c5a60f0d678603529f38d3c762b0d37b2397c
BFH93 <60694673+BFH92@users.noreply.github.com>
2021-10-04 13:10:55 +0200

    push readme.md - 2021-10-04 13:10:55 +0200

--- push_on_github.rb
+++ push_on_github.rb
@@ -1,7 +1,8 @@
 require 'dotenv'
 Dotenv.load('.env')
-url = "https://#{ENV['TOKEN']}@github.com/BFH92/git_push_automation_exercice.git"
+
 def push_on_github(date, file_name)
+  url = "https://#{ENV['TOKEN']}@github.com/BFH92/git_push_automation_exercice.git"
   system('git add .')
   system("git commit -m 'push #{file_name} - #{date}'")
   system("git remote set-url origin #{url}")
