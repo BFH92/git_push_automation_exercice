3bbaba934b1088f8688fca5bf3192152b58a1ae8
BFH93 <60694673+BFH92@users.noreply.github.com>
2021-10-04 13:06:46 +0200

    push readme.md - 2021-10-04 13:06:46 +0200

--- add_text_to_readme.rb
+++ add_text_to_readme.rb
@@ -9,7 +9,7 @@ def add_text_to_readme(date, file_name)
   file.write("\n1/ Installation d'un cron \n")
   file.write("\n$crontab -e \n ")
   file.write("\ninsérer ensuite la ligne suivante en spécifiant le chemin où se trouve le repo que vous avez cloné\n")
-  file.write("\n* * * * * /bin/bash -l -c 'cd /Users/Path/to/your/directory && ruby ~/your_directory/index.rb' &> /Users/Path/to/your/directory/cron.txt\n ")
+  file.write("\n * * * * * /bin/bash -l -c 'cd /Users/Path/to/your/directory && ruby ~/your_directory/index.rb' &> /Users/Path/to/your/directory/cron.txt\n ")
   file.write("\nremplacer les '* * * * *' pour ajuster la fréquence de push. Vous pouvez tester le format ici => https://crontab.tech/\n")
   
   file.write("\n2/ création de la variable d'environnement Token, pour permettre le push via un gitHub token\n")
@@ -17,7 +17,7 @@ def add_text_to_readme(date, file_name)
   file.write("\najouter la variable TOKEN='Votre tokenGitHub'\n ")
   file.write("\ninformations sur le gitHub Token ici => https://techglimpse.com/git-push-github-token-based-passwordless/'\n ")
   file.write("\n$installation de la gem dotenv\n ")
-  file.write("\n$ gem install dotenv\n ")
+  file.write("\n$gem install dotenv\n ")
   
   file.write("\nEt vous êtes bons ! \n ")
   
--- readme.md
+++ readme.md
@@ -9,7 +9,7 @@ $crontab -e
  
 insérer ensuite la ligne suivante en spécifiant le chemin où se trouve le repo que vous avez cloné
 
-* * * * * /bin/bash -l -c 'cd /Users/Path/to/your/directory && ruby ~/your_directory/index.rb' &> /Users/Path/to/your/directory/cron.txt
+ * * * * * /bin/bash -l -c 'cd /Users/Path/to/your/directory && ruby ~/your_directory/index.rb' &> /Users/Path/to/your/directory/cron.txt
  
 remplacer les '* * * * *' pour ajuster la fréquence de push. Vous pouvez tester le format ici => https://crontab.tech/
 
@@ -23,10 +23,10 @@ informations sur le gitHub Token ici => https://techglimpse.com/git-push-github-
  
 $installation de la gem dotenv
  
-$ gem install dotenv
+$gem install dotenv
  
 Et vous êtes bons ! 
  
 -----Metrics-----
 
-dernière contribution automatique - 2021-10-04 13:06:04 +0200
+dernière contribution automatique - 2021-10-04 13:06:46 +0200
