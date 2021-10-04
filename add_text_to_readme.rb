17af22b481c524a5d7bc67d7d57fc154e7386ae8
BFH93 <60694673+BFH92@users.noreply.github.com>
2021-10-04 13:06:04 +0200

    push readme.md - 2021-10-04 13:06:04 +0200

--- add_text_to_readme.rb
+++ add_text_to_readme.rb
@@ -8,20 +8,20 @@ def add_text_to_readme(date, file_name)
   
   file.write("\n1/ Installation d'un cron \n")
   file.write("\n$crontab -e \n ")
-  file.write("insérer ensuite la ligne suivante en spécifiant le chemin où se trouve le repo que vous avez cloné")
-  file.write("* * * * * /bin/bash -l -c 'cd /Users/Path/to/your/directory && ruby ~/your_directory/index.rb' &> /Users/Path/to/your/directory/cron.txt\n ")
-  file.write("remplacer les '* * * * *' pour ajuster la fréquence de push. Vous pouvez tester le format ici => https://crontab.tech/\n")
+  file.write("\ninsérer ensuite la ligne suivante en spécifiant le chemin où se trouve le repo que vous avez cloné\n")
+  file.write("\n* * * * * /bin/bash -l -c 'cd /Users/Path/to/your/directory && ruby ~/your_directory/index.rb' &> /Users/Path/to/your/directory/cron.txt\n ")
+  file.write("\nremplacer les '* * * * *' pour ajuster la fréquence de push. Vous pouvez tester le format ici => https://crontab.tech/\n")
   
-  file.write("2/ création de la variable d'environnement Token, pour permettre le push via un gitHub token\n")
-  file.write("création d'un fichier .env\n ")
-  file.write("ajouter la variable TOKEN='Votre tokenGitHub'\n ")
-  file.write("informations sur le gitHub Token ici => https://techglimpse.com/git-push-github-token-based-passwordless/'\n ")
-  file.write("$installation de la gem dotenv\n ")
-  file.write("$ gem install dotenv\n ")
+  file.write("\n2/ création de la variable d'environnement Token, pour permettre le push via un gitHub token\n")
+  file.write("\ncréation d'un fichier .env\n ")
+  file.write("\najouter la variable TOKEN='Votre tokenGitHub'\n ")
+  file.write("\ninformations sur le gitHub Token ici => https://techglimpse.com/git-push-github-token-based-passwordless/'\n ")
+  file.write("\n$installation de la gem dotenv\n ")
+  file.write("\n$ gem install dotenv\n ")
   
-  file.write("Et vous êtes bons ! \n ")
+  file.write("\nEt vous êtes bons ! \n ")
   
-  file.write("-----Metrics-----")
-  file.write("\ndernière contribution automatique - #{date}")
+  file.write("\n-----Metrics-----\n")
+  file.write("\ndernière contribution automatique - #{date}\n")
   file.close
 end
--- readme.md
+++ readme.md
@@ -6,14 +6,27 @@ Installation
 1/ Installation d'un cron 
 
 $crontab -e 
- insérer ensuite la ligne suivante en spécifiant le chemin où se trouve le repo que vous avez cloné* * * * * /bin/bash -l -c 'cd /Users/Path/to/your/directory && ruby ~/your_directory/index.rb' &> /Users/Path/to/your/directory/cron.txt
- remplacer les '* * * * *' pour ajuster la fréquence de push. Vous pouvez tester le format ici => https://crontab.tech/
+ 
+insérer ensuite la ligne suivante en spécifiant le chemin où se trouve le repo que vous avez cloné
+
+* * * * * /bin/bash -l -c 'cd /Users/Path/to/your/directory && ruby ~/your_directory/index.rb' &> /Users/Path/to/your/directory/cron.txt
+ 
+remplacer les '* * * * *' pour ajuster la fréquence de push. Vous pouvez tester le format ici => https://crontab.tech/
+
 2/ création de la variable d'environnement Token, pour permettre le push via un gitHub token
+
 création d'un fichier .env
- ajouter la variable TOKEN='Votre tokenGitHub'
- informations sur le gitHub Token ici => https://techglimpse.com/git-push-github-token-based-passwordless/'
- $installation de la gem dotenv
- $ gem install dotenv
- Et vous êtes bons ! 
- -----Metrics-----
-dernière contribution automatique - 2021-10-04 13:05:21 +0200
+ 
+ajouter la variable TOKEN='Votre tokenGitHub'
+ 
+informations sur le gitHub Token ici => https://techglimpse.com/git-push-github-token-based-passwordless/'
+ 
+$installation de la gem dotenv
+ 
+$ gem install dotenv
+ 
+Et vous êtes bons ! 
+ 
+-----Metrics-----
+
+dernière contribution automatique - 2021-10-04 13:06:04 +0200
