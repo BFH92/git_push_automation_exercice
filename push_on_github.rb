80e0118befa9974fe0477ce6898d00be8a10b5b7
BFH93 <60694673+BFH92@users.noreply.github.com>
2021-10-04 12:40:23 +0200

    push readme.md - 2021-10-04 12:40:23 +0200

--- push_on_github.rb
+++ push_on_github.rb
@@ -1,11 +1,8 @@
 require 'dotenv'
 Dotenv.load('.env')
 def push_on_github(date, file_name)
-  username = ENV['USERNAME']
-  password = ENV['PASSWWORD']
-  puts password
   system('git add .')
   system("git commit -m 'push #{file_name} - #{date}'")
-  system("git remote set-url origin 'https://BFH92:i%^a)Md&Y!7{@github.com/BFH92/git_push_automation_exercice.git'")
+  system("git remote set-url origin 'https://#{ENV['TOKEN']}@github.com/BFH92/git_push_automation_exercice.git'")
   system("git push origin main")
 end
